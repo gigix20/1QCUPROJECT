33c298d03c3023f3dff0b29695fc7465c3be6524
Rico <villete.leonardo.buya@gmail.com>
2026-02-20 19:13:22 +0800

    Added index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+header("Location: views/landing_page.php");
+exit;
+
+// Redirect to landing page
+// Redirect to login page if you are not logged in
+// For easier URL typing, just type localhost/1QCUPROJECT on browser
